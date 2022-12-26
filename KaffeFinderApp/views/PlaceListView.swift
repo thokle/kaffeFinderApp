122c771175bc4cb97886db878981c468a8f54c05
Thomas Kleist <thomas_kleist@tdcadsl.dk>
2022-12-26 20:09:37 +0100

    added contentViiew to build

--- KaffeFinderApp/views/PlaceListView.swift
+++ KaffeFinderApp/views/PlaceListView.swift
@@ -9,18 +9,23 @@ import SwiftUI
 
 struct PlaceListView: View {
     @State var netWork: NetWorkService = NetWorkService()
+    
     @State var places:[SalePlace] = [SalePlace]()
+    
     var body: some View {
-       
-        List(places, id: \.id) {
-            SalePlacDetailsView(salePlace: $0)
+      
+       List(places, id: \.id) {
+        
+           SalePlacDetailsView(salePlace: $0)
         }.onAppear(perform:loadSalePlaces)
     }
 }
 
 struct PlaceListView_Previews: PreviewProvider {
+
     static var previews: some View {
-        PlaceListView()
+   
+        PlaceListView(places: [SalePlace()])
     }
 }
 
--- KaffeFinderApp/views/SalePlaceView.swift
+++ KaffeFinderApp/views/SalePlaceView.swift
@@ -83,7 +83,7 @@ struct SalePlaceView: View {
     
     struct SalePlaceView_Previews: PreviewProvider {
         static var previews: some View {
-            SalePlaceView(location: LocationManager())
+            SalePlaceView(location: LocationManager()).environmentObject(ViewModel())
         }
     }
     
